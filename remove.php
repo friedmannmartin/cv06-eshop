2cb6bd98b172f5712c84f3d8365bb31a0e733f80
Martin Friedmann <friedmann_martin@outlook.com>
2020-04-02 00:10:17 +0200

    Opravení odebírání položek z košíku

--- remove.php
+++ remove.php
@@ -6,9 +6,13 @@
 
   #region nalezení zboží s daným ID a jeho odebrání z košíku
   $id = $_GET['id'];
-  foreach ($_SESSION['cart'] as $key => $value){
-    if ($value == $id) {
-      unset($_SESSION['cart'][$key]);
+  foreach ($_SESSION['cart'] as $key => $value) {
+    if ($key == $id) {
+      if($value == 1) {
+        unset($_SESSION['cart'][$key]);
+      } else {
+        $_SESSION['cart'][$key]--;
+      }
     }
   }
   //pro zjednodušení nekontrolujeme, jestli opravdu bylo dané zboží v košíku
