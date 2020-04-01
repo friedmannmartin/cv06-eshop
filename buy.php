9fc5ba6c445e4aef0c5ecb78e569179a1fbb7960
Martin Friedmann <friedmann_martin@outlook.com>
2020-04-01 23:55:05 +0200

    Změna formátu vkládání zboží do session košíku

--- buy.php
+++ buy.php
@@ -10,16 +10,21 @@
   #endregion vytvoření session pole pro košík
 
   #region kontrola, jestli je zboží v DB
-  $stmt = $db->prepare("SELECT * FROM goods WHERE id=?");
+  $stmt = $db->prepare('SELECT * FROM goods WHERE id=?');
   $stmt->execute(array($_GET['id']));
   $goods = $stmt->fetch();
 
   if (!$goods){
-    die("Unable to find goods!");//TODO místo ukočení skriptu by tu bylo hezké jen nějaké uložení chybové hlášky
+    die('Unable to find goods!');//TODO místo ukočení skriptu by tu bylo hezké jen nějaké uložení chybové hlášky
   }
   #endregion kontrola, jestli je zboží v DB
 
-  $_SESSION['cart'][] = $goods["id"];//přidání ID zboží do košíku
-  //TODO neresime, ze od jednoho zbozi muze byt vetsi mnozstvi nez 1, domaci ukol :)
+  #region přidání zboží do košíku
+    if(array_key_exists($goods['id'], $_SESSION['cart'])){
+      $_SESSION['cart'][$goods['id']]++; //zvýšení množství zboží které už je v košíku
+    }else {
+      $_SESSION['cart'][$goods['id']] = 1; //přidání zboží které ještě není v košíku
+    }
+  #endregion přidání zboží do košíku
 
   header('Location: cart.php');//přesměrujeme uživatele na košík
